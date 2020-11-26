a14baf5f525c630909a42b0b388b86ba474e07d8
adrianagers <adrianageraldine.19@gmail.com>
2020-11-26 10:40:45 -0500

    registro de user

--- src/app/Components/registro-empresa/registro-empresa.component.ts
+++ src/app/Components/registro-empresa/registro-empresa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-registro-empresa',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEmpresaComponent implements OnInit {
 
-  constructor() { }
+  singUpForm: FormGroup ; //permite indical las validaciones  que se crearan
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { this.validator(); }
 
   ngOnInit(): void {
   }
+  validator(){
+    this.singUpForm = this.formBuilder.group({
+      nameCompany: ['', Validators.required],
+      nitCompany: ['', [ Validators.required, Validators.minLength(9)]],
+      email: ['', [ Validators.required, Validators.email ]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['Company', Validators.required],
+      phoneCompany: ['', Validators.required],
+      namepush: ['', Validators.required]
 
+    });
+
+  }
+  saveCompany(){
+    if (this.singUpForm.valid){
+      alert('Se registro exitosamente');
+    }else{
+      alert('El formulario no es valido ');
+    }
+  }
 }
--- src/app/Components/registro-user/registro-user.component.ts
+++ src/app/Components/registro-user/registro-user.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup} from '@angular/forms';
-
+import { UserService } from '../../Services/user.service' ;
 @Component({
   selector: 'app-registro-user',
   templateUrl: './registro-user.component.html',
   styleUrls: ['./registro-user.component.css']
 })
 export class RegistroUserComponent implements OnInit {
-  singUpForm: FormGroup ; //permite indical las validaciones  que se crearan
+  singUpForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
-  ) { }
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) { this.validator(); }
 
   ngOnInit(): void {
   }
   validator(){
-    // this.singUpForm = this.formBuilder.group()
+    this.singUpForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [ Validators.required, Validators.email ]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['User', Validators.required],
+      phone: ['']
+
+    });
+  }
+  saveUser(){
+    if (this.singUpForm.valid){
+      console.log(this.singUpForm.value);
+      this.userService.createUser( this.singUpForm.value ).subscribe(
+        (userCreated) => {
+          console.log(userCreated);
+          alert('Usuario creado correctamente');
+        },
+        (error) => {
+          console.error('Tuvimos un error --> ', error);
+        }
+      );
+    }else{
+      alert('El formulario no es valido ');
+    }
   }
 
 }
